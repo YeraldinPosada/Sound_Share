1a7ff63c7c3116019905b8b592afb5d978c8e660
YeraldinPosada <yeposadag@unal.edu.co>
2026-03-24 11:57:48 -0500

    feat(auth): implement user authentication using Sanctum

---
+++ gateway/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+#Rutas para la Autentificación de usuarios
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
